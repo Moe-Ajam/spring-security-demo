bfb13711913968474433d4ec6ebd525e9250556c
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-03-07 17:04:30 +0300

    Added configuration for InMemoryDetailsManager Implementation.
    Added PasswordEncoder Implementation.

---
+++ src/main/java/com/moe/springsecuritydemo/config/ProjectConfig.java
@@ -0,0 +1,34 @@
+package com.moe.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("moe")
+                .password("123")
+                .authorities("read")
+                .build();
+
+        UserDetails user2 = User.withUsername("test")
+                .password("123")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, user2);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
