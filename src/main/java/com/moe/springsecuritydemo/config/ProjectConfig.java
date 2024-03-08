5406efd6fb61a745e7a6f70d794532341e8da780
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2024-03-08 15:03:04 +0300

    Added my own implementation of AuthenticationProvider.

--- src/main/java/com/moe/springsecuritydemo/config/ProjectConfig.java
+++ src/main/java/com/moe/springsecuritydemo/config/ProjectConfig.java
@@ -1,16 +1,38 @@
 package com.moe.springsecuritydemo.config;
 
+import com.moe.springsecuritydemo.service.CustomAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class ProjectConfig {
+    @Configuration
+    @RequiredArgsConstructor
+    public class ProjectConfig {
+
+        private final CustomAuthenticationProvider authenticationProvider;
+
+        @Bean
+        SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+            http.httpBasic(Customizer.withDefaults());
+
+            http.authorizeHttpRequests(
+                    c -> c.anyRequest().authenticated()
+            );
+
+            http.authenticationProvider(authenticationProvider);
+
+            return http.build();
+        }
 
     @Bean
     UserDetailsService userDetailsService() {
