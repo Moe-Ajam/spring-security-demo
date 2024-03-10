56f64f3c6c2dba86604c83701ac066cc92c9dddb
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2024-03-11 00:03:26 +0300

    Added custom userDetailsService Implementation.

--- src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
+++ src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,6 +30,17 @@ public class WebAuthorizationConfig {
         //http.authenticationProvider(authenticationProvider);
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUsernameQuery = "select username, authority from authorities where username = ?";
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUsernameQuery);
+        return userDetailsManager;
     }
 
 }
