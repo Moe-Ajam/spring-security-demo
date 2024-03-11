0cde8a36d4f12d5027f702b8c2f7f116af791cbc
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2024-03-12 00:41:54 +0300

    Added more filters...

--- src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
+++ src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
@@ -1,42 +1,28 @@
 package com.moe.springsecuritydemo.config;
 
+import com.moe.springsecuritydemo.filter.AuthenticationLoggingFilter;
 import com.moe.springsecuritydemo.filter.RequestValidationFilter;
+import com.moe.springsecuritydemo.filter.StaticKeyAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebAuthorizationConfig {
 
+    private final StaticKeyAuthenticationFilter filter;
+
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.addFilterBefore(
-                new RequestValidationFilter(), BasicAuthenticationFilter.class)
-                .authorizeRequests(c -> c.anyRequest().permitAll());
+        http.addFilterAt(filter, BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
 
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
-        String authsByUsernameQuery = "select username, authority from authorities where username = ?";
-
-        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUsernameQuery);
-        return userDetailsManager;
     }
 
 }
