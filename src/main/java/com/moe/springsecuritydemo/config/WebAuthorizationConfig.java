00d26cc50f9bd77d1c6555fd25ba25b07dbe3ca7
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-03-11 15:16:27 +0300

    Added first custom filter.

--- src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
+++ src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
@@ -1,28 +1,24 @@
 package com.moe.springsecuritydemo.config;
 
+import com.moe.springsecuritydemo.filter.RequestValidationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebAuthorizationConfig {
 
-    //private final CustomAuthenticationProvider authenticationProvider;
-
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.httpBasic(Customizer.withDefaults());
-
-        http.authorizeHttpRequests(
-                c -> c.anyRequest().authenticated()
-        );
-
-        //http.authenticationProvider(authenticationProvider);
+        http.addFilterBefore(
+                new RequestValidationFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests(c -> c.anyRequest().permitAll());
 
         return http.build();
     }
