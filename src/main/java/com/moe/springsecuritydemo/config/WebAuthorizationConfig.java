c1c6d7e08eeb49d803bd4c9cf4889da5f011c782
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2024-03-15 21:38:50 +0300

    Played around with request matchers

--- src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
+++ src/main/java/com/moe/springsecuritydemo/config/WebAuthorizationConfig.java
@@ -3,6 +3,7 @@ package com.moe.springsecuritydemo.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,11 +15,19 @@ public class WebAuthorizationConfig {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
+        http.csrf(
+                c->c.disable()
+        );
+
         http.httpBasic(Customizer.withDefaults());
 
         http.authorizeHttpRequests(
-                c -> c.requestMatchers("/hello").hasRole("ADMIN")
-                        .requestMatchers("/ciao").hasRole("MANAGER")
+                c -> c.requestMatchers(HttpMethod.GET, "/a")
+                        .authenticated()
+                        .requestMatchers(HttpMethod.POST, "/a")
+                        .permitAll()
+                        .anyRequest()
+                        .denyAll()
         );
 
         return http.build();
